3fff952be071de6bc6e5be4da7e005285f370e72
Catherine "Catrina" Nowakowski <nowakowskicatherine@gmail.com>
2025-10-07 03:27:25 -0400

    adding an automated article card

---
+++ article_card.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+    $.getJSON('articles.json', function(articles) {
+        const container = $('#article-cards-container');
+        
+        $.each(articles, function(index, article) {
+            const cardHtml = `
+            <div class="col-md-4 mb-4"> <article class="all-browsers">
+                    <h1>${article.title}</h1>
+                    <article class="browser">
+                        <h2>${article.subtitle}</h2>
+                        <img class="article_img img-fluid" src="${article.imageSrc}" alt="${article.altText}">
+                        <p>${article.description}</p>
+                        <a href="${article.link}">
+                            <button class="btn btn-primary button1">View</button>
+                        </a>
+                    </article>
+                </article>
+            </div>
+        `;
+        
+            container.append(cardHtml);
+        });
+    });
+});
