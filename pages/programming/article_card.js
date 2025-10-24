b92e536fb540e783745a03e43a4e75d1c92f9546
Catherine "Catrina" Nowakowski <nowakowskicatherine@gmail.com>
2025-10-24 14:54:39 -0400

    Update article_card.js
    color update

--- pages/programming/article_card.js
+++ pages/programming/article_card.js
@@ -5,11 +5,11 @@ $(document).ready(function() {
         $.each(articles, function(index, article) {
             const cardHtml = `
             <div class="col-md-4 mb-4"> <article class="all-browsers">
-                    <h1>${article.title}</h1>
+                    <h1 style = "color: #333;">${article.title}</h1>
                     <article class="browser">
-                        <h2>${article.subtitle}</h2>
+                        <h2 style = "color: #333;">${article.subtitle}</h2>
                         <img class="article_img img-fluid" src="${article.imageSrc}" alt="${article.altText}">
-                        <p>${article.description}</p>
+                        <p style = "color: #333;">${article.description}</p>
                         <a href="${article.link}">
                             <button class="btn btn-primary button1">View</button>
                         </a>
