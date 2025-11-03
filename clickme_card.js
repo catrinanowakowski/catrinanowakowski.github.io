9300d9e475673167064b8bb44c778c2a1bf20de9
Catherine "Catrina" Nowakowski <nowakowskicatherine@gmail.com>
2025-11-03 14:44:13 -0500

    Formatting improvements

--- clickme_card.js
+++ clickme_card.js
@@ -7,11 +7,9 @@ $(document).ready(function() {
             <div class="col-md-4 mb-4"> 
             <article class="all-browsers">
                     <h1 style = "color: #333;">${clickme.title}</h1>
-                    <article class="browser">
                         <a href="${clickme.link}" target="_blank">
                             <button class="btn btn-primary button1">View</button>
                         </a>
-                    </article>
                 </article>
             </div>
         `;
